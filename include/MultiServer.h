4fb8d15f766f629465c65bc3fb5642fac8ee966d
yqm-307 <9793365432@qq.com>
2022-04-01 14:05:18 +0800

    4/1

---
+++ include/MultiServer.h
@@ -0,0 +1,9 @@
+#pragma once
+/*
+    逻辑服务器
+*/
+#include <iostream>
+#include <boost/asio.hpp>
+
+
+
---
+++ include/User.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <iostream>
+
+
+class User
+{
+public:
+
+private:  
+    int uid_;   //唯一uid
+    //char* token;  //未使用
+    int roomid_;    //所属房间
+    
+};
